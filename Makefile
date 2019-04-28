f81581a78c73d2af28e49fa6219cca430bff7429
Jan-Erik Rediger <janerik@fnordig.de>
2019-04-28 13:06:29 +0200

    REWRITE LOADER IN RUST!!!1!

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
-all: stdshout_preload.so target/release/stdshout
+all: preload/target/release/stdshout_preload.so target/release/stdshout
 
 target/release/stdshout: src/bin/stdshout.rs
 	cargo build --release
 
 
-stdshout_preload.so: stdshout_preload.c
-	gcc -O2 -o $@ $< -fpic -shared -ldl -lc
+preload/target/release/stdshout_preload.so: preload/src/lib.rs
+	cd preload && cargo build --release
 
 
 run: all
-	@bash -c 'STDSHOUT_EXE=$$(readlink -f target/release/stdshout) LD_PRELOAD=./stdshout_preload.so $${SHELL}'
+	@bash -c 'STDSHOUT_EXE=$$(readlink -f target/release/stdshout) LD_PRELOAD=./preload/target/release/stdshout_preload.so $${SHELL}'
---
+++ preload/src/lib.rs
@@ -0,0 +1,77 @@
+use std::env;
+use std::ffi::CString;
+use std::fs::read_link;
+use std::io::{stdin, stdout};
+use std::os::unix::io::AsRawFd;
+
+use ctor::ctor;
+use nix::unistd::{close, dup, dup2, execv, fork, ForkResult, pipe};
+
+#[ctor]
+fn reopen_stdout() {
+    let stdshout = match env::var("STDSHOUT_EXE") {
+        Ok(var) => {
+            if var.len() > 0 {
+                var
+            } else {
+                eprintln!("Can't find stdshout exe. Set STDSHOUT_EXE to the full path.");
+                return;
+            }
+        }
+        _ => {
+            eprintln!("Can't find stdshout exe. Set STDSHOUT_EXE to the full path.");
+            return;
+        }
+    };
+
+    let linkname = match read_link("/proc/self/exe") {
+        Err(_) => {
+            eprintln!("Can't read /proc/self/exe");
+            return;
+        }
+        Ok(path) => format!("{}", path.display())
+    };
+
+    if linkname == stdshout {
+        // We are stdshout already.
+        return;
+    }
+
+    let stdout = stdout();
+    let old_stdout = dup(stdout.as_raw_fd()).unwrap();
+
+    let (read, write) = match pipe() {
+        Ok((read, write)) => (read, write),
+        Err(_) => {
+            eprintln!("Can't open pipe");
+            return;
+        }
+    };
+
+    if let Err(_) = dup2(write, stdout.as_raw_fd()) {
+        eprintln!("Can't dup write fd");
+        return;
+    }
+
+    match fork() {
+        Err(_) => {
+            eprintln!("Can't fork");
+            return;
+        },
+        Ok(ForkResult::Child) => {
+            let _ = close(write);
+
+            let _ = dup2(read, stdin().as_raw_fd());
+            let _ = dup2(old_stdout, stdout.as_raw_fd());
+
+            let args = [CString::new("stdshout").unwrap()];
+            let path = CString::new(stdshout).unwrap();
+            let _ = execv(&path, &args[..]);
+            eprintln!("Something went wrong");
+        },
+        Ok(ForkResult::Parent { .. }) => {
+            close(read).unwrap();
+        }
+    }
+
+}
